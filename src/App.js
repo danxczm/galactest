0420249f40c0cf3393f487f04525f14b95d827f9
dan <81313913+danxczm@users.noreply.github.com>
2024-08-10 12:45:19 +0300

    feat: setting up routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,52 @@
-function App() {
-  return <div className="text">galactica</div>;
-}
+import { Routes, Route, NavLink } from 'react-router-dom';
+
+const App = () => {
+  const buildLinkClass = ({ isActive }) => {
+    return { color: `${isActive ? 'green' : 'black'}` };
+  };
+
+  return (
+    <div>
+      <nav>
+        <NavLink to="/" style={buildLinkClass}>
+          Home
+        </NavLink>
+        <NavLink to="/about" style={buildLinkClass}>
+          About
+        </NavLink>
+        <NavLink to="/services" style={buildLinkClass}>
+          Services
+        </NavLink>
+      </nav>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <h1>Home</h1>
+            </>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <>
+              <h1>About us</h1>
+            </>
+          }
+        />
+        <Route
+          path="/services"
+          element={
+            <>
+              <h1>Services</h1>
+            </>
+          }
+        />
+        <Route path="*" element={<>404</>} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
