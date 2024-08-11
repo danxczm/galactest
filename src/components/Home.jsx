af9e1767fce31988a99fd9063bc3e16c935f94a4
dan <81313913+danxczm@users.noreply.github.com>
2024-08-11 17:02:55 +0300

    feat: marquee implmemented

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import consulting from '../images/consulting.svg';
+import traffic from '../images/traffic.svg';
 
 const Home = () => {
   return (
@@ -23,6 +24,22 @@ const Home = () => {
             </div>
           </div>
         </div>
+      </div>
+      <div className="marquee">
+        <ul className="marquee__inner-right marquee__inner-golden">
+          {Array.from({ length: 8 }).map((_, i) => (
+            <li key={i} className={`marquee__item item-${i + 1}`}>
+              {i % 2 === 0 ? <p>WE BUY TRAFFIC</p> : <img src={traffic} alt="WE BUY TRAFFIC" />}
+            </li>
+          ))}
+        </ul>
+        <ul className="marquee__inner-left marquee__inner-purple">
+          {Array.from({ length: 8 }).map((_, i) => (
+            <li key={i} className={`marquee__item item-${i + 1}`}>
+              {i % 2 === 0 ? <p>WE BUY TRAFFIC</p> : <img src={traffic} alt="WE BUY TRAFFIC" />}
+            </li>
+          ))}
+        </ul>
       </div>
     </main>
   );
