6360745f2882e5f190876027040582075dc4d53d
dan <81313913+danxczm@users.noreply.github.com>
2024-08-12 11:07:47 +0300

    feat: expect useLayoutEffect work better

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,14 +1,16 @@
+import { useLayoutEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import contentMap from '../utils/pageContent';
 import AnimationPage from './AnimationPage';
-import { useMemo } from 'react';
 
 const Page = () => {
   const { pathname } = useLocation();
   const page = pathname.replace('/', '');
+  const [content, setContent] = useState(null);
 
-  const content = useMemo(() => {
-    return contentMap[page]?.content || null;
+  useLayoutEffect(() => {
+    const newContent = contentMap[page]?.content || null;
+    setContent(newContent);
   }, [page]);
 
   return (
